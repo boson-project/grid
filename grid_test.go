fc72a3111aa9a859f55fb32366ec0ddad1e192cb
Lance Ball <lball@redhat.com>
2020-07-17 15:22:15 -0400

    test: add simple endpoint tests for events and subscriptions (#1)
    * test: add simple endpoint tests for events and subscriptions
    
    This commit adds two simple integration tests for the events and subscription
    endpoints, ensuring that they exist. Also, to reduce code duplication, extracted
    a `StartGrid()` function for reuse in multiple tests.
    
    Also, stubbed a unit test for the version handler.

--- grid_test.go
+++ grid_test.go
@@ -8,6 +8,30 @@ import (
 	"github.com/boson-project/grid"
 )
 
+func StartGrid(t *testing.T) (g *grid.Grid, err error) {
+	t.Helper()
+
+	listening := make(chan bool)
+	errCh := make(chan error)
+
+	g = grid.New(
+		grid.WithAddress("127.0.0.1:"),                  // OS-chosen port
+		grid.WithOnListen(func() { listening <- true }), // signal start
+	)
+
+	go func() {
+		if err := g.Serve(context.Background()); err != nil {
+			errCh <- err
+		}
+	}()
+
+	select {
+	case err = <-errCh:
+	case <-listening:
+	}
+	return g, err
+}
+
 // TestCancel ensures the service starts and stops without error with all defaults using
 // a cancelable context.
 func TestStart(t *testing.T) {
@@ -25,22 +49,42 @@ func TestStart(t *testing.T) {
 
 // TestVersion ensures that the /v1/version endpoint returns the version structure.
 func TestVersion(t *testing.T) {
-	listening := make(chan bool)
-
-	g := grid.New(
-		grid.WithAddress("127.0.0.1:"),                  // OS-chosen port
-		grid.WithOnListen(func() { listening <- true }), // signal start
-	)
-
-	go func() {
-		if err := g.Serve(context.Background()); err != nil {
-			t.Fatal(err)
-		}
-	}()
-
-	<-listening // Wait for start signal
+	g, err := StartGrid(t)
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	res, err := http.Get("http://" + g.Addr().String() + "/v1/version")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if res.StatusCode != 200 {
+		t.Fatalf("Expected HTTP 200, got %v", res.StatusCode)
+	}
+}
+
+func TestEventsEndpointAvailable(t *testing.T) {
+	g, err := StartGrid(t)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	res, err := http.Get("http://" + g.Addr().String() + "/v1/events")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if res.StatusCode != 200 {
+		t.Fatalf("Expected HTTP 200, got %v", res.StatusCode)
+	}
+}
+
+func TestSubscriptionsEndpointAvailable(t *testing.T) {
+	g, err := StartGrid(t)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	res, err := http.Get("http://" + g.Addr().String() + "/v1/subscriptions")
 	if err != nil {
 		t.Fatal(err)
 	}
--- handler_test.go
+++ handler_test.go
@@ -1,3 +1,24 @@
 package grid
 
+import (
+	"net/http/httptest"
+	"testing"
+)
+
+func TestVersionHandler(t *testing.T) {
+	g := New(
+		WithAddress("127.0.0.1:"),
+	)
+
+	req := httptest.NewRequest("GET", "http://127.0.0.1/version", nil)
+	w := httptest.NewRecorder()
+	g.handleVersion(w, req)
+
+	resp := w.Result()
+
+	if resp.StatusCode != 200 {
+		t.Error("Expected status code 200")
+	}
+}
+
 // TODO: handler unit tests using httptest
